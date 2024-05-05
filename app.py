21523e85d5e78e9e5e04468e9839768934abc30c
Siddhant Sharma <siddhantsharma97@gmail.com>
2024-05-05 10:46:47 +0530

    fixed errors

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import chainlit as cl
 from src.llm import ask_bot
-
+from src.config import instruction
 @cl.on_message
 async def main(message: cl.Message):
-    response=ask_bot(message.content)
+    response=ask_bot(message.content,instruction)
     
     await cl.Message(
         content=f"{response}",
--- src/llm.py
+++ src/llm.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from src.config import instruction
-
+from langchain_core.messages import HumanMessage, SystemMessage
 load_dotenv()
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
@@ -11,13 +11,17 @@ os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 message=[{"role": "system", "content":instruction}]
 
-def ask_bot(user_message):
-    messages=[{"role": "system", "content":instruction }, 
-            {"role": "user", "content": user_message}]  
-
-    llm = ChatGoogleGenerativeAI(model="gemini-pro")
-        
-    response = llm.invoke(messages)
+def ask_bot(user_message,instruction):
     
-    return response.content
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+    
+    
+    respones=model(
+    [
+        SystemMessage(content=instruction),
+        HumanMessage(content=user_message),
+    ]
+)
+    
+    return respones.content
 
